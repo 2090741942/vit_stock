4dfe834238e6048c24a651f607b3ba1cd08ca29a
2090741942 <2090741942@qq.com>
2026-03-19 11:54:43 +0000

    message

--- env.sh
+++ env.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+env_name='vit'
+
+source "/workspace/miniforge/etc/profile.d/conda.sh"
+
+# ===== 1. 如果环境不存在才创建 =====
+if ! conda env list | grep -q "^${env_name} "; then
+    echo "Creating env: ${env_name}"
+    conda create -n "${env_name}" python=3.10 -y
+else
+    echo "Env ${env_name} already exists"
+fi
+
+# ===== 2. 激活环境 =====
+conda activate "${env_name}"
+
+# conda create -n "${env_name}" python=3.10 -y
+
+# conda activate "${env_name}"
+
+# ==== 3. 安装库 ====
 PYTHON_BIN=${1:-python}
 
 echo "Using Python: $PYTHON_BIN"
